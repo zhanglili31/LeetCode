930735da5ebdddbd93d2bfa8aea5ff338893533d
zhanglili31 <zhanglili31@meituan.com>
2021-01-24 04:28:05 +0800

    使用暴力方法解决53题

---
+++ src/main/java/com/zhanglili31/LeetCode/Q53_最大子序和/Solution.java
@@ -0,0 +1,33 @@
+package com.zhanglili31.LeetCode.Q53_最大子序和;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] a = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        Solution solution = new Solution();
+        int res = solution.maxSubArray(a);
+        System.out.println(res);
+    }
+
+    //    使用暴力的方法，将所有的连续的子数组全部找出来，并且求和找出最大的出来
+//    但是这字方式：超出时间限制
+    public int maxSubArray(int[] nums) {
+        int maxValue = nums[0];
+        int temp = 0;
+        for (int i = nums.length; i > 0; i--) {//每轮循环中包含的子数组的个数
+            for (int j = 0; j <= nums.length - i; j++) {//每轮循环的起始位置
+                for (int k = 0; k < i; k++) {//k代表每次求和的连续子数组
+                    temp += nums[k + j];
+                }
+                if (temp > maxValue) {
+                    maxValue = temp;
+                }
+                temp = 0;
+            }
+
+        }
+        return maxValue;
+
+    }
+
+}
---
+++ src/test/java/com/zhanglili31/LeetCode/Q53/Violent.java
@@ -0,0 +1,13 @@
+package com.zhanglili31.LeetCode.Q53;
+import com.zhanglili31.LeetCode.Q53_最大子序和.Solution;
+
+public class Violent  {
+
+    public static void main(){
+        int []a={-2,1,-3,4,-1,2,1,-5,4};
+        Solution solution=new Solution();
+        int res=solution.maxSubArray(a);
+        System.out.println(res);
+
+    }
+}
