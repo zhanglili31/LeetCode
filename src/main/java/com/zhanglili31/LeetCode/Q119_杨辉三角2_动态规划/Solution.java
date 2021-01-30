b76731bcd38b7657457ad74e95aa891ccd6b877b
zhanglili31 <zhanglili31@meituan.com>
2021-01-30 19:33:27 +0800

    解决溢出的问题

--- src/main/java/com/zhanglili31/LeetCode/Q119_杨辉三角2_动态规划/Solution.java
+++ src/main/java/com/zhanglili31/LeetCode/Q119_杨辉三角2_动态规划/Solution.java
@@ -1,16 +1,17 @@
 package com.zhanglili31.LeetCode.Q119_杨辉三角2_动态规划;
-
 import java.util.ArrayList;
 import java.util.List;
 //c(k,i)=c(k,i-1)*(k-i+1)/i
 public class Solution {
     public List<Integer> getRow(int rowIndex) {
-        int k=rowIndex;
-        List<Integer>l=new ArrayList<Integer>();
+        int k = rowIndex;
+        List<Integer> l = new ArrayList<Integer>();
         l.add(1);
+        long pre = 1;
         for (int i = 1; i <= rowIndex; i++) {
-
-            l.add(l.get(i-1)*(k-i+1)/i);
+            long cur = pre * (k - i + 1) / i;
+            l.add((int) cur);
+            pre = cur;
         }
         return l;
     }
