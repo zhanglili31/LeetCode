b58c2d684b864422fc94c8fd5b42801007026ee7
zhanglili <1137596624@qq.com>
2022-03-12 20:07:28 +0800

    543

--- src/main/java/com/zhanglili31/leetCode/editor/cn/[494]目标和.java
+++ src/main/java/com/zhanglili31/leetCode/editor/cn/[494]目标和.java
@@ -1,36 +1,53 @@
 package com.zhanglili31.leetCode.editor.cn;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import com.zhanglili31.leetCode.editor.cn.utils.CreateTree;
+import com.zhanglili31.leetCode.editor.cn.utils.TreeNode;
 
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    int res=0;
-    public int findTargetSumWays(int[] nums, int target) {
-        recursion(nums, 0, target, 0);
-        return res;
+//494 目标和
+//2022-03-12 19:54:36
+class TargetSum {
 
+    public static void main(String[] args) {
+        Solution solution = new TargetSum().new Solution();
+        TreeNode root = CreateTree.deserialize("[1,1,1,1,1,0,0,0,1,null,0,1,1,null,1,null,null,null,null,null,null,null,null,null,null,null,null]");
+        int[][] a = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        String testStr = "hello";
+        int temp[] = {-1, 0, 1, 2, -1, -4};
+        System.out.println(solution.findTargetSumWays(temp,10));
     }
-    //深度遍历，
-    void recursion(int[] nums, int index, int target,int curVal) {
-        int len = nums.length;
-        if(index==len){
-            if (curVal==target) {
-                res++;
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        int res = 0;
+
+        public int findTargetSumWays(int[] nums, int target) {
+            recursion(nums, 0, target, 0);
+            return res;
+
+        }
+
+        //深度遍历，
+        void recursion(int[] nums, int index, int target, int curVal) {
+            int len = nums.length;
+            if (index == len) {
+                if (curVal == target) {
+                    res++;
+                }
+                return;
             }
-            return;
+            recursion(nums, index + 1, target, curVal + nums[index]);
+            recursion(nums, index + 1, target, curVal - nums[index]);
         }
-        recursion(nums, index + 1, target, curVal + nums[index]);
-        recursion(nums, index + 1, target, curVal - nums[index]);
-    }
 
-    //动态规划 dp[ i ][ j ] = dp[ i - 1 ][ j - nums[ i ] ] + dp[ i - 1 ][ j + nums[ i ] ]
-    void useDp(int nums[], int target) {
-        int dp[][] = new int[1001][1001];
-        for (int i = 0; i < nums.length; i++) {
+        //动态规划 dp[ i ][ j ] = dp[ i - 1 ][ j - nums[ i ] ] + dp[ i - 1 ][ j + nums[ i ] ]
+        void useDp(int nums[], int target) {
+            int dp[][] = new int[1001][1001];
+            for (int i = 0; i < nums.length; i++) {
 
+            }
         }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
+
+
+}
--- src/main/java/com/zhanglili31/leetCode/editor/cn/[543]二叉树的直径.java
+++ src/main/java/com/zhanglili31/leetCode/editor/cn/[543]二叉树的直径.java
@@ -1,64 +1,59 @@
-package com.zhanglili31.leetCode.editor.cn;//给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
-//
-// 
-//
-// 示例 : 
-//给定二叉树 
-//
-//           1
-//         / \
-//        2   3
-//       / \     
-//      4   5    
-// 
-//
-// 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。 
-//
-// 
-//
-// 注意：两结点之间的路径长度是以它们之间边的数目表示。 
-// 👍 942 👎 0
+package com.zhanglili31.leetCode.editor.cn;
 
-
-//leetcode submit region begin(Prohibit modification and deletion)
-
+import com.zhanglili31.leetCode.editor.cn.utils.CreateTree;
 import com.zhanglili31.leetCode.editor.cn.utils.TreeNode;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-//todo 调试通过
-class Solution0 {
-    int maxLength=0;
-    public int diameterOfBinaryTree(TreeNode root) {
-        recursion(root);
-        if(root==null)
-            return 0;
-        diameterOfBinaryTree(root.left);
-        diameterOfBinaryTree(root.right);
-        return maxLength;
+//543 二叉树的直径
+//2022-03-12 19:56:05
+class DiameterOfBinaryTree {
+
+    public static void main(String[] args) {
+        Solution solution = new DiameterOfBinaryTree().new Solution();
+        TreeNode root = CreateTree.deserialize("[1,2,3,4,5,8,4,9,5,1,2,5,4,1,5,6,2,14]");
+        int[][] a = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        String testStr = "hello";
+        int temp[] = {-1, 0, 1, 2, -1, -4};
+        System.out.println(solution.diameterOfBinaryTree(root));
     }
-    public int recursion(TreeNode node) {
-        if(node==null)return -1;
-        int left = recursion(node.left);
-        int right = recursion(node.right);
-        if(left+right>maxLength){
-            maxLength = left + right;
-            System.out.println(maxLength);
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        int maxLength = 0;
+
+        public int diameterOfBinaryTree(TreeNode root) {
+            recursion(root);
+            return maxLength;
         }
-        return Math.max(left, right) + 1;
+
+        public int recursion(TreeNode node) {
+            if (node == null)
+                return -1;
+            int left = recursion(node.left)+1;
+            int right = recursion(node.right)+1;
+            if (left + right > maxLength) {
+                maxLength = left + right;
+                System.out.println(maxLength);
+            }
+            return Math.max(left, right);
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
+
+
+}
