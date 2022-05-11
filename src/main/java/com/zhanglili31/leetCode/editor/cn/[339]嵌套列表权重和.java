4b84efb24b3a0a7a2c30e01e75344fef7911910c
zhanglili <1137596624@qq.com>
2022-05-11 13:42:43 +0800

    339

---
+++ src/main/java/com/zhanglili31/leetCode/editor/cn/[339]嵌套列表权重和.java
@@ -0,0 +1,78 @@
+package com.zhanglili31.leetCode.editor.cn;
+
+import com.zhanglili31.leetCode.editor.cn.utils.CreateTree;
+import com.zhanglili31.leetCode.editor.cn.utils.TreeNode;
+import com.zhanglili31.leetCode.editor.cn.utils.CreateArray;
+
+import java.util.Arrays;
+
+//339 嵌套列表权重和
+//2022-05-11 13:26:52
+class NestedListWeightSum {
+
+    public static void main(String[] args) {
+//        Solution solution = new NestedListWeightSum().new Solution();
+        TreeNode root = CreateTree.deserialize("[1,2,3,4,5,6,7,8,9]");
+        int[][] a = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        String testStr = "hello";
+        String strArray[] = {"wrt", "wrf"};
+        char[] charArray = {'t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e'};
+        int temp[] = CreateArray.getArray(20, 100);
+        System.out.println(Arrays.toString(temp));
+//        System.out.println(solution);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * // This is the interface that allows for creating nested lists.
+     * // You should not implement it, or speculate about its implementation
+     * public interface NestedInteger {
+     * // Constructor initializes an empty nested list.
+     * public NestedInteger();
+     * <p>
+     * // Constructor initializes a single integer.
+     * public NestedInteger(int value);
+     * <p>
+     * // @return true if this NestedInteger holds a single integer, rather than a nested list.
+     * public boolean isInteger();
+     * <p>
+     * // @return the single integer that this NestedInteger holds, if it holds a single integer
+     * // Return null if this NestedInteger holds a nested list
+     * public Integer getInteger();
+     * <p>
+     * // Set this NestedInteger to hold a single integer.
+     * public void setInteger(int value);
+     * <p>
+     * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+     * public void add(NestedInteger ni);
+     * <p>
+     * // @return the nested list that this NestedInteger holds, if it holds a nested list
+     * // Return empty list if this NestedInteger holds a single integer
+     * public List<NestedInteger> getList();
+     * }
+     */
+/*    class Solution {
+        int ans = 0;
+
+        public int depthSum(List<NestedInteger> nestedList) {
+            return depthSum(nestedList, 1);
+
+        }
+
+        public int depthSum(List<NestedInteger> nestedList, int depth) {
+            int sum=0;
+            for (NestedInteger n : nestedList) {
+                if (n.isInteger()) {
+                    sum+=n.getInteger()*depth;
+                }else{
+                    sum += depthSum(n.getList(), depth + 1);
+                }
+            }
+            return sum;
+
+        }
+    }*/
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
