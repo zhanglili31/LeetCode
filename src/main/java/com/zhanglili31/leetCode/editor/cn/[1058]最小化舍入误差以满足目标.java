484e5d157790c6ea1c57902eed1c525f5116c2e8
zhanglili <1137596624@qq.com>
2022-06-08 06:22:35 +0800

    1058

--- src/main/java/com/zhanglili31/leetCode/editor/cn/[1058]最小化舍入误差以满足目标.java
+++ src/main/java/com/zhanglili31/leetCode/editor/cn/[1058]最小化舍入误差以满足目标.java
@@ -5,6 +5,7 @@ import com.zhanglili31.leetCode.editor.cn.utils.TreeNode;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Comparator;
 
 //1058 最小化舍入误差以满足目标
 //2022-06-04 02:15:32
@@ -15,19 +16,23 @@ class MinimizeRoundingErrorToMeetTarget {
         TreeNode root = CreateTree.deserialize("[1,2,3,4,5,6,7,8,9]");
         int[][] a = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
         String testStr = "hello";
-        String strArray[] = {"2.146","2.656","2.908","2.739","2.589"};
+        String strArray[] = {"0.700","2.800","4.900"};
         char[] charArray = {'t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e'};
 //        int temp[] = CreateArray.getArray(20, 100);
         int temp[] = {5, 3, 2, 2};
         System.out.println(Arrays.toString(temp));
-        System.out.println(solution.minimizeError(strArray, 11));
+        System.out.println(solution.minimizeError(strArray, 8));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
+
+    //参考思路：https://leetcode.cn/problems/minimize-rounding-error-to-meet-target/solution/greedy-by-lizju/
     class Solution {
         int FLOOR = 0;
         int CEIL = 1;
         String ans = "-1";
+
+
 
         public String minimizeError(String[] prices, int target) {
             int v[][] = new int[prices.length][2];
@@ -48,19 +53,27 @@ class MinimizeRoundingErrorToMeetTarget {
             if (target < min || target > max) {
                 return ans;
             }
-            int k = max-target;
+            int k = target-min;
+            Arrays.sort(d, new Comparator<BigDecimal[]>() {
+                @Override
+                public int compare(BigDecimal[] o1, BigDecimal[] o2) {
+                    return o2[0].compareTo(o1[0]);
+                }
+            });
             BigDecimal res = new BigDecimal(0);
+            //先取k个1
             for (int i = 0; i < k; i++) {
-                res = res.add(d[i][FLOOR]);
-            }
-            for (int i = k; i < d.length; i++) {
                 res = res.add(d[i][CEIL]);
             }
+            // 剩下的全部选择0
+            for (int i = k; i < d.length; i++) {
+                res = res.add(d[i][FLOOR]);
+            }
+
             ans= res.toString();
             if (!ans.contains(".")) {
                 ans = ans + ".000";
             }
-//            DFS(v, d, 0, target, new BigDecimal(0), 0);
             return ans;
         }
         //DFS超时
