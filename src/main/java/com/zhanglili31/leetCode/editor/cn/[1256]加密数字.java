57b606f2d59fa02aaa5960357c1ebf276eab1971
zhanglili <1137596624@qq.com>
2022-06-11 22:53:49 +0800

    1265

---
+++ src/main/java/com/zhanglili31/leetCode/editor/cn/[1256]加密数字.java
@@ -0,0 +1,33 @@
+package com.zhanglili31.leetCode.editor.cn;
+
+import com.zhanglili31.leetCode.editor.cn.utils.CreateTree;
+import com.zhanglili31.leetCode.editor.cn.utils.TreeNode;
+
+import java.util.Arrays;
+
+//1256 加密数字
+//2022-06-11 22:38:31
+class EncodeNumber {
+
+    public static void main(String[] args) {
+        Solution solution = new EncodeNumber().new Solution();
+        TreeNode root = CreateTree.deserialize("[1,2,3,4,5,6,7,8,9]");
+        int[][] a = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        String testStr = "hello";
+        String strArray[] = {"wrt", "wrf"};
+        char[] charArray = {'t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e'};
+//        int temp[] = CreateArray.getArray(20, 100);
+        int temp[] = {5, 3, 2, 2};
+        System.out.println(Arrays.toString(temp));
+        System.out.println(solution);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String encode(int num) {
+            return Integer.toBinaryString(num + 1).substring(1);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/com/zhanglili31/leetCode/editor/cn/[1265]逆序打印不可变链表.java
@@ -0,0 +1,45 @@
+package com.zhanglili31.leetCode.editor.cn;
+
+import com.zhanglili31.leetCode.editor.cn.utils.CreateTree;
+import com.zhanglili31.leetCode.editor.cn.utils.TreeNode;
+
+import java.util.Arrays;
+//1265 逆序打印不可变链表
+//2022-06-11 22:50:13
+class PrintImmutableLinkedListInReverse {
+
+    public static void main(String[] args) {
+//        Solution solution = new PrintImmutableLinkedListInReverse().new Solution();
+        TreeNode root = CreateTree.deserialize("[1,2,3,4,5,6,7,8,9]");
+        int [][]a={{1,1,1},{1,0,1},{1,1,1}};
+        String testStr="hello";
+        String strArray[] = {"wrt","wrf"};
+        char[] charArray = {'t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e'};
+//        int temp[] = CreateArray.getArray(20, 100);
+        int temp[] = {5, 3, 2, 2};
+        System.out.println(Arrays.toString(temp));
+//        System.out.println(solution);
+    }
+    
+        //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * interface ImmutableListNode {
+ *     public void printValue(); // print the value of this node.
+ *     public ImmutableListNode getNext(); // return the next node.
+ * };
+ */
+
+/*class Solution {
+    public void printLinkedListInReverse(ImmutableListNode head) {
+        if (head == null) {
+            return;
+        }
+        printLinkedListInReverse(head.getNext());
+        head.printValue();
+    }
+}*/
+//leetcode submit region end(Prohibit modification and deletion)
+ 
+}
